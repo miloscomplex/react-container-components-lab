c1b49b86213068349c2ff7e9fa97ea87c20bd849
Michael Sutton <hicallmesutton@gmail.com>
2021-03-08 12:03:05 -0600

    commit tree 2852262d2bb5130c23ff0c93a8e685d878a6e2e9

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,11 @@
 // Code MovieReviews Here
+import React, { Component } from 'react'
+
+const MovieReviews = props => {
+  return (
+    <ul className='review-list'>
+      {props.reviews.map( (review, index) => <li key={index} className='review'> {review.display_title}</li>)}
+    </ul>
+  )
+}
+export default MovieReviews
